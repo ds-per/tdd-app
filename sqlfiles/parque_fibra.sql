d36a25cc4e7a3211dcca09a52fc33c27d3ff0523
Ivan Pereira <ivan@plugidea.pt>
2015-02-02 19:22:19 +0100

    fix fibra

--- sqlfiles/parque_fibra.sql
+++ sqlfiles/parque_fibra.sql
@@ -13,12 +13,14 @@ FROM
     ( select max(cc.idconsumo) as maxidconsumo
             from consumo cc
             inner join subscricao on subscricao.idsubscricao = cc.idsubscricao
+            inner join produto on produto.idproduto = subscricao.idproduto
             where cc.datainicio <= %(day)s
-            and idproduto > 27
+            and produto.idprodutotipo = 3 and produto.genero = "M"
             group by subscricao.idcontaservico
     ) as xx
     inner join consumo c1 on c1.idconsumo = xx.maxidconsumo
     INNER JOIN subscricao s1 ON ( s1.idsubscricao = c1.idsubscricao )
+    inner join produto p on p.idproduto = s1.idproduto
     INNER JOIN contaservico ct ON ct.idcontaservico = s1.idcontaservico
     INNER JOIN conta using(idconta)
     LEFT JOIN equipamento equip ON equip.idcontaservico = ct.idcontaservico and equip.idtipoequipamento > 1
@@ -26,4 +28,5 @@ FROM
     INNER JOIN comuna cm on cm.idcomuna = m.idcomuna
     INNER JOIN municipio mp on mp.idmunicipio = cm.idmunicipio
        WHERE
-        s1.idproduto > 27
+        ct.idtipocontaservico = 2
+        and p.idprodutotipo = 3 and p.genero = "M"
