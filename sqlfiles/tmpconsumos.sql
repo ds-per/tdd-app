ba621c8633e197df127bc562585306e86abce957
Ivan Pereira <ivan@plugidea.pt>
2015-02-10 14:47:24 +0100

    fix tmpconsumos query

--- sqlfiles/tmpconsumos.sql
+++ sqlfiles/tmpconsumos.sql
@@ -1,6 +1,6 @@
-SELECT  
+SELECT
  equip.idmodelomaterial as idmodelomaterialstb
-, idprovincia 
+, idprovincia
 , mp.idmunicipio as idmunicipio
 , s2.idproduto AS 'from'
 , s1.idproduto AS 'to'
@@ -12,16 +12,20 @@ SELECT
             (s2.idproduto = 24 AND s1.idproduto = 23) OR
             (s2.idproduto = 22 AND s1.idproduto = 23), 'UPGRADE', 'NOVO'))) AS tipo
 , DATEDIFF(c1.datainicio, c2.datafim) AS demora
-, if(crs.idvoucher is not null, itd.idloja,if(crs.idfacturaitem is not null, aquisicao.idloja, 6)) as loja
+, if(crs.idvoucher is not null, itd.idloja,
+        if(crs.idfacturaitem is not null, aquisicao.idloja,
+        if(crs.idcarregamentodirecto is not null, ctd.idloja, 8))) as loja
 , caixa_itd.idloja as loja_caixa
-, if(crs.idvoucher is not null, voucher.activacao, if(crs.idfacturaitem is not null, 'LOJA', 'INTERNO')) as canal
+, if(crs.idvoucher is not null, voucher.activacao,
+        if(crs.idfacturaitem is not null, 'LOJA',
+        if(crs.idcarregamentodirecto is not null, 'SIRE', 'INTERNO'))) as canal
 , if(conta.contahotel = 1, COUNT(*), count(DISTINCT ct.idcontaservico)) as `fact_count`
         FROM consumo c1
         INNER JOIN subscricao s1 ON ( s1.idsubscricao = c1.idsubscricao )
         LEFT JOIN consumo c2 ON ( c2.idconsumo = (
                 SELECT MAX(idconsumo)
                 FROM consumo c3 inner JOIN subscricao s3 using(idsubscricao)
-                WHERE c3.idconsumo < c1.idconsumo 
+                WHERE c3.idconsumo < c1.idconsumo
                 AND s3.idcontaservico = s1.idcontaservico
                 AND s3.idproduto != 27
         ))
@@ -49,6 +53,7 @@ SELECT
     LEFT JOIN itemdisponivel caixa_itd ON caixa_itd.iditemdisponivel = caixa_it.iditemdisponivel
     LEFT JOIN facturaitem fi ON fi.iditem = crs.idfacturaitem
     LEFT JOIN aquisicao ON aquisicao.idfactura = fi.idfactura
+    LEFT JOIN carregamentodirecto ctd ON ctd.idcarregamentodirecto = crs.idcarregamentodirecto
     WHERE
     c1.datainicio = %(day)s
     and s1.idproduto != 27
