1877f7ce19a5addb7ff457bf7c6f67dae391e57e
Ivan Pereira <ivan@plugidea.pt>
2015-04-30 18:50:26 +0100

    acerto dos consumos

--- sqlfiles/consumos.sql
+++ sqlfiles/consumos.sql
@@ -1,4 +1,4 @@
-SELECT  
+SELECT
  equip.idmodelomaterial as modelomaterial
 , idprovincia as provincia
 , mp.idmunicipio as municipio
@@ -27,22 +27,26 @@ SELECT
         FROM consumo c1
         INNER JOIN subscricao s1 ON ( s1.idsubscricao = c1.idsubscricao )
         LEFT JOIN consumo c2 ON ( c2.idconsumo = (
-                SELECT MAX(idconsumo)
-                FROM consumo c3 inner JOIN subscricao s3 using(idsubscricao)
-                WHERE c3.idconsumo < c1.idconsumo 
-                AND s3.idcontaservico = s1.idcontaservico
-                AND s3.idproduto != 27
+                SELECT max(idconsumo) from consumo c4
+                inner join subscricao s4 using(idsubscricao)
+                where c4.datafim = (select max(c3.datafim)
+                                    FROM consumo c3 inner JOIN subscricao s3 using(idsubscricao)
+                                     WHERE c3.datafim < c1.datainicio
+                                      AND s3.idcontaservico = s1.idcontaservico
+                                      AND s3.idproduto != 27
+                                    )
+                and s4.idcontaservico = s1.idcontaservico
         ))
     LEFT JOIN subscricao s2 ON  s2.idsubscricao = c2.idsubscricao
     INNER JOIN contaservico ct ON ct.idcontaservico =  s1.idcontaservico
-    INNER join conta using(idconta)
-    INNER JOIN equipamento equip ON equip.idcontaservico = ct.idcontaservico
+    INNER JOIN conta using(idconta)
+    INNER JOIN subscricao_equipamento on s1.idsubscricao = subscricao_equipamento.idsubscricao
+    INNER JOIN equipamento equip ON equip.idequipamento = subscricao_equipamento.idequipamento
     INNER JOIN morada m ON m.idmorada = ct.idmorada
     INNER JOIN comuna cm ON cm.idcomuna = m.idcomuna
     INNER JOIN municipio mp ON mp.idmunicipio = cm.idmunicipio
     WHERE
     c1.datainicio = %(day)s
     and s1.idproduto != 27
-    and equip.idtipoequipamento = 1
-    and (equip.dataanulacao is null or equip.dataanulacao > %(day)s )
-    GROUP BY demora, idmodelomaterial, s1.idproduto, idprovincia, municipio, tipo
+    and ct.idtipocontaservico = 1
+    GROUP BY demora, idmodelomaterial, from_produto, to_produto, idprovincia, municipio, tipo
