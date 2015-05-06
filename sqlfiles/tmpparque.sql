bafe73474b198a1b8962fa995fe165704afe8779
Ivan Pereira <ivan@plugidea.pt>
2015-05-06 12:26:19 +0100

    change tmpparque.sql to collect data fom fct_consumos

--- sqlfiles/tmpparque.sql
+++ sqlfiles/tmpparque.sql
@@ -1,14 +1,14 @@
-SELECT 
+SELECT
 p.idprovincia AS provincia
 , m.idmunicipio AS municipio
       , sum(IF(fc.tipo='EXPIRADO', fc.fact_count, 0)) AS Churn
       , sum(IF(fc.tipo='NOVO', fc.fact_count, 0)) AS GrossAdds
       , sum(IF(fc.demora>1, fc.fact_count, 0)) AS Reactivacoes
-      , (SELECT sum(fp.fact_count) FROM fct_parque fp WHERE fp.idtempo = %(idtempo)s AND fp.municipio = m.idmunicipio) 
-          - (SELECT sum(fp.fact_count) FROM fct_parque fp WHERE fp.idtempo = %(idtempo)s  AND fp.municipio = m.idmunicipio) AS NetAdds
+      , (SELECT sum(fp.fact_count) FROM fct_parque fp WHERE fp.idtempo = %(idtempo)s AND fp.municipio = m.idmunicipio)
+          - (SELECT sum(fp.fact_count) FROM fct_parque fp WHERE fp.idtempo = %(yest)s  AND fp.municipio = m.idmunicipio) AS NetAdds
       , (SELECT sum(fp.fact_count) FROM fct_parque fp WHERE fp.idtempo = %(idtempo)s AND fp.municipio = m.idmunicipio) AS Parque
             FROM
                 municipio m
                 INNER JOIN provincia p ON p.idprovincia = m.idprovincia
-                LEFT JOIN tmp_fct_consumos fc ON fc.idtempo = %(idtempo)s AND fc.idmunicipio = m.idmunicipio
+                LEFT JOIN fct_consumos fc ON fc.idtempo = %(idtempo)s AND fc.municipio = m.idmunicipio
             GROUP BY p.idprovincia, m.idmunicipio
